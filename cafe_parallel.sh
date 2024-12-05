1f78d15e2b848141ee041861ea94012836df3be7
Júlia Crespin Esteve <98484251+jcrespinesteve@users.noreply.github.com>
2024-12-05 16:54:02 +0100

    Update cafe_parallel.sh

--- cafe_parallel.sh
+++ cafe_parallel.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #SBATCH --array=0-2 # The indices you are passing to your script, inclusive, e.g. 0-2 means we run 3 Jobs
-#SBATCH --partition=big # Normal Jobs should use partition work
-#SBATCH --nodes=1 # Nodes per Job
-#SBATCH --ntasks=1 # Tasks per Job
-#SBATCH --cpus-per-task 64 # CPUs per Task, Increase this if your computation uses multiprocessing
-#SBATCH --mem-per-cpu 16G # Memory per CPU, Increase this if your computation is memory heavy
-#SBATCH --job-name=CAFE_p # Give a meaningful Job name
-#SBATCH --time 16:00:00 # Maximum run-time
-#SBATCH -o out-%A-%a.out # Logs are saved in the folder slurmlogs
-#SBATCH -e err-%A-%a.out # Logs are saved in the folder slurmlogs
+#SBATCH --partition=big 
+#SBATCH --nodes=1 
+#SBATCH --ntasks=1 
+#SBATCH --cpus-per-task 64 
+#SBATCH --mem-per-cpu 16G 
+#SBATCH --job-name=CAFE_P
+#SBATCH --time 16:00:00 
+#SBATCH -o out-%A-%a.out 
+#SBATCH -e err-%A-%a.out 
 
 echo Running Task ID $SLURM_ARRAY_TASK_ID
 
