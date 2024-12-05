4db7393fc5bdc72b09f741d2dc1074e23b647300
Júlia Crespin Esteve <98484251+jcrespinesteve@users.noreply.github.com>
2024-12-05 16:53:14 +0100

    Update and rename cafe_2003.sh to cafe.sh

--- cafe.sh
+++ cafe.sh
@@ -4,9 +4,9 @@
 #SBATCH --cpus-per-task=64
 #SBATCH --partition=big
 #SBATCH --mem-per-cpu 30G
-#SBATCH --job-name=CAFE_03
+#SBATCH --job-name=CAFE
 #SBATCH --time=17:00:00
-#SBATCH --output=2003.out
-#SBATCH --error=2003.err
+#SBATCH --output=cafe.out
+#SBATCH --error=cafe.err
 
-python CAFE_2003.py
+python run_cafe.py
